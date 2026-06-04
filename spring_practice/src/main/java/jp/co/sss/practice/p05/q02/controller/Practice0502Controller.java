f2acafd2da00babe4d26a30ce8a196ad43ba1acf
narumina <narumina@C010422720.zipangu.cac.co.jp>
2026-06-04 23:35:06 +0900

    practice05-02完了しました。

--- spring_practice/src/main/java/jp/co/sss/practice/p05/q02/controller/Practice0502Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/q02/controller/Practice0502Controller.java
@@ -1,6 +1,27 @@
 package jp.co.sss.practice.p05.q02.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sss.practice.p05.repository.FruitsSeasonRepository;
+
 // Practice6-01
+@Controller
 public class Practice0502Controller {
+
+	@Autowired
+	FruitsSeasonRepository fruitsSeasonRepository;
+
+	//	一覧画面表示用
+	@RequestMapping(path = "/fruits/list/sort/season")
+	public String showFruitsListSortBySeason(Model model) {
+		// DBからデータを取得し、seasonMonthの昇順で並べ替えてリクエストスコープに保存
+		model.addAttribute("fruitsList", fruitsSeasonRepository.findAllByOrderBySeasonMonthAsc());
+
+		return "practice05/02/fruits_list";
+
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
@@ -1,9 +1,14 @@
 package jp.co.sss.practice.p05.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jp.co.sss.practice.p05.entity.FruitsSeason;
 
 public interface FruitsSeasonRepository extends JpaRepository<FruitsSeason, Integer> {
+
+	//昇順に並べ替え昇順の（Asc）は省略可能
+	List<FruitsSeason> findAllByOrderBySeasonMonthAsc();
 
 }
